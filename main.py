2a02107ea90c6435f974d07d196a6f7c4f1df686
Lautharo <thiagolautharoapas123@gmail.com>
2025-05-23 00:52:52 -0300

    v1.0.0

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox as mb
+
+def richar():
+    mb.showinfo("Richar", "es muy puton mariconazo")
+
+
---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+hola = "HELLO WOLRD"
+
+EDAD = 0
